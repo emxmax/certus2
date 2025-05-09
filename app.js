97f0c775e976bcad324b362bc09b8c4d2296b581
Max Carrasco <max.carrasco.h@gmail.com>
2025-05-09 09:44:22 -0500

    modificar el .env

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(express.json());
 app.use(cors());
 
-const MONGO_URI = process.MONGO_URI;
+const MONGO_URI = process.env.MONGO_URI;
 
 mongoose.connect(MONGO_URI).then(()=> {
     console.log('Se conecto exitosamente...');
