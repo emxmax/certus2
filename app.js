988427e5eb7b4be5760d8b8118735fa0a19e13a1
Max Carrasco <max.carrasco.h@gmail.com>
2025-05-09 09:40:15 -0500

    agrege .env

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -31,7 +32,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use(express.json());
 app.use(cors());
 
-const MONGO_URI = 'mongodb+srv://mcarrascoh:lolos12345@cluster0.4jhpswq.mongodb.net/certus?retryWrites=true&w=majority&appName=Cluster0'
+const MONGO_URI = process.MONGO_URI;
 
 mongoose.connect(MONGO_URI).then(()=> {
     console.log('Se conecto exitosamente...');
